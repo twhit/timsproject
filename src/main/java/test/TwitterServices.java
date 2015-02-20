922c3bfc1a5d5b9de0cbb138a8c4a2acbcbb87a7
twhit <whitfield.tim.53@gmail.com>
2015-02-20 18:12:14 -0500

    whatever

--- src/main/java/test/TwitterServices.java
+++ src/main/java/test/TwitterServices.java
@@ -20,8 +20,8 @@ import test.DB;
 
 @Path("/twitter")
 public class TwitterServices {
-	String consumerKey = "";
-	String consumerSecret = "";
+	String consumerKey = "9g6hAg3P1oQDoDtGeYeC5Tw23";
+	String consumerSecret = "HIwWoF3U2b7kiG2SW6OFbP6hOpyI1SYsCCBhXvIdU012TnushT";
 
 	@GET
 	@Path("/request")
