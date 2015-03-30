4c51a4a02ac2e3b04f2f9fea53e52b1aae1cad83
twhit <whitfield.tim.53@gmail.com>
2015-03-30 02:00:59 -0400

    no python

--- src/main/java/test/UploadTestPhoto.java
+++ src/main/java/test/UploadTestPhoto.java
@@ -18,7 +18,6 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.FileUtils;
 import javax.servlet.annotation.WebServlet;
-import org.springframework.*;
 //import org.springframework.context.ApplicationContext;
 //import org.springframework.web.context.support.WebApplicationContextUtils;
 
